dc1a74f219902f8943d0a884de8562eccad81ce5
Dodogari <gaori926@gmail.com>
2023-08-14 22:00:41 +0900

    change set image

--- app/src/main/java/com/example/goldenratio/CocktailInterface.kt
+++ app/src/main/java/com/example/goldenratio/CocktailInterface.kt
@@ -7,9 +7,25 @@ import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface CocktailInterface {
-    //칵테일 전체 조회
+    //칵테일 조회 - 전체
     @GET("/golden-ratio/cocktail/all")
-    fun getCocktailList():Call<ArrayList<BoardData>>
+    fun getCocktailAll():Call<ArrayList<BoardData>>
+
+    //칵테일 조회 - 별점순
+    @GET("/golden-ratio/cocktail/star")
+    fun getCocktailStar():Call<ArrayList<BoardData>>
+
+    //칵테일 조회 - 좋아요순
+    @GET("/golden-ratio/cocktail/like")
+    fun getCocktailLike():Call<ArrayList<BoardData>>
+
+    //칵테일 조회 - 도수높은순
+    @GET("/golden-ratio/cocktail/alchol")
+    fun getCocktailAlcohol():Call<ArrayList<BoardData>>
+
+    //칵테일 조회 - 단맛높은순
+    @GET("/golden-ratio/cocktail/sweet")
+    fun getCocktailSweet():Call<ArrayList<BoardData>>
 
     //숙취해소 전체 조회
     @GET("/golden-ratio/hangover/all")
