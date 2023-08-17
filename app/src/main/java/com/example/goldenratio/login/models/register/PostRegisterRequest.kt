b94ad97b8b6e7c91d806d6c01343f27549f27509
youjin09222 <68536115+youjin09222@users.noreply.github.com>
2023-08-17 22:51:05 +0900

    [Profile] 이미지 url 변경

--- app/src/main/java/com/example/goldenratio/login/models/register/PostRegisterRequest.kt
+++ app/src/main/java/com/example/goldenratio/login/models/register/PostRegisterRequest.kt
@@ -1,10 +1,11 @@
 package com.example.goldenratio.login.models.register
 
 import com.google.gson.annotations.SerializedName
+import java.net.URL
 
 data class PostRegisterRequest(
         @SerializedName("userId") val id: String,
         @SerializedName("password") val password: String,
         @SerializedName("nickname") val nickname: String,
-        @SerializedName("profileImageUrl") val url: String
+        @SerializedName("profileImageUrl") val url: URL?
 )
