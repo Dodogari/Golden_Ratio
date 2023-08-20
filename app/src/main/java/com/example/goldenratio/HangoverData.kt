c0041f72824ba689a0f1c9d42e54aaa62d5cf0b1
Dodogari <gaori926@gmail.com>
2023-08-20 20:53:07 +0900

    수정본

--- app/src/main/java/com/example/goldenratio/HangoverData.kt
+++ app/src/main/java/com/example/goldenratio/HangoverData.kt
@@ -1,13 +1,16 @@
 package com.example.goldenratio
 
 import com.example.GradientData
+import com.google.gson.annotations.SerializedName
 
-data class HangoverData(var title: String,
-                        var thumbnail: Int,
-                        var rating: Float,
-                        var like: Int,
-                        var likeCheck: Boolean,
-                        var writtenDate: String,
-                        var gradientList: ArrayList<GradientData>,
-                        var recipeContent: String,
-                        var reviewList: ArrayList<ReviewData>)
+data class HangoverData(@SerializedName("id") var id: Int,
+                        @SerializedName("title") var title: String,
+                        @SerializedName("content") val content: String,
+                        @SerializedName("mainImage") var mainImage: String,
+                        @SerializedName("category") var category: String,
+                        @SerializedName("averageScore") var averageScore: Float,
+                        @SerializedName("likes") var likes: Int,
+                        @SerializedName("gradientList") var gradientList: ArrayList<GradientData>,
+                        @SerializedName("reviews") var reviews: ArrayList<ReviewData>,
+                        @SerializedName("createdDate") var createdDate: ArrayList<Int>,
+                        @SerializedName("lastModifiedTime") var lastModifiedTime: ArrayList<Int>)
