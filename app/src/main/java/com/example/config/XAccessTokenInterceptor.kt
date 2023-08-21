5f9582c3e5fafbbfb3f51e6f7acc80b6d28aa7bf
youjin09222 <68536115+youjin09222@users.noreply.github.com>
2023-08-22 02:09:51 +0900

    [Server] 연결

--- app/src/main/java/com/example/config/XAccessTokenInterceptor.kt
+++ app/src/main/java/com/example/config/XAccessTokenInterceptor.kt
@@ -1,8 +1,6 @@
 package com.example.config
 
-
-import com.example.config.ApplicationClass.Companion.X_ACCESS_TOKEN
-import com.example.config.ApplicationClass.Companion.sSharedPreferences
+import com.example.goldenratio.login.accessToken
 import okhttp3.Interceptor
 import okhttp3.Request
 import okhttp3.Response
@@ -10,11 +8,12 @@ import java.io.IOException
 
 class XAccessTokenInterceptor : Interceptor {
     @Throws(IOException::class)
+
     override fun intercept(chain: Interceptor.Chain): Response {
         val builder: Request.Builder = chain.request().newBuilder()
-        val jwtToken: String? = sSharedPreferences.getString(X_ACCESS_TOKEN, null)
-        if (jwtToken != null) {
-            builder.addHeader("X-ACCESS-TOKEN", jwtToken)
+        //val jwtToken: String? = sSharedPreferences.getString(X_ACCESS_TOKEN, null)
+        if (accessToken != null) {
+            builder.addHeader("Authorization", "Bearer " + accessToken.toString())
         }
         return chain.proceed(builder.build())
     }
